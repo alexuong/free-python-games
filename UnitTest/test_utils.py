2ee4768de4de2aed92f61d07ec56ab27bd1186bf
Wbprgmr <blomgres@oregonstate.edu>
2020-02-27 09:26:42 -0800

    Update test_utils.py

--- UnitTest/test_utils.py
+++ UnitTest/test_utils.py
@@ -5,8 +5,10 @@ Unit testing of utils.py
 import os
 import sys
 import unittest
-from UnitTest.testLib import md5
-from freegames.utils import floor
+from UnitTest.testLib import md5, winOrLin
+from freegames.utils import floor, path
+
+slash, isWin = winOrLin()
 
 
 class utilsTestCase(unittest.TestCase):
@@ -20,7 +22,7 @@ class utilsTestCase(unittest.TestCase):
             md5_act = md5(path)
         self.assertEqual(md5_exp, md5_act)
     
-    def test_floor(self):
+    def test_utils_floor(self):
         # Positive Test
         exp = 0.0
         act = floor(10.0, 100.0)
@@ -33,3 +35,22 @@ class utilsTestCase(unittest.TestCase):
         exp = 200.0
         act = floor(200.0, 100.0)
         self.assertEqual(exp, act)
+    
+    def test_utils_path(self):
+        # Positive Test
+        cwd = os.getcwd()
+        testFile = 'pacman.py'
+        expPath = cwd + slash+ 'freegames' + slash + testFile
+        actPath = path(testFile)
+        self.assertEqual(expPath, actPath)
+    
+    def test_utils_line(self):
+        # TODO: add tests that use Turtle
+        pass
+    
+    def test_utils_square(self):
+        # TODO: add tests that use Turtle
+        pass
+
+
+# Vector Class
