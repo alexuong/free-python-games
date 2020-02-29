6a55bff4d55e7747f39a465d138bc78fc19dc337
Ryan Kirkpatrick <kirkpary@oregonstate.edu>
2020-02-28 19:34:19 -0800

    Finished adding vector tests
    Added tests for eq, ne, iadd, add, isub, sub, imul, mul, scale, itruediv, truediv, neg, abs, rotate, and repr.

--- UnitTest/test_utils.py
+++ UnitTest/test_utils.py
@@ -105,3 +105,200 @@ class utilsTestCase(unittest.TestCase):
         self.assertEquals(exp_x, c.x)
         self.assertEquals(exp_y, c.y)
         
+    def test_utils_eq(self):
+        act = vector(1, 2)
+        w = vector(1, 2)
+        z = vector(1, 3)
+        x = vector(3, 2)
+        self.assertEquals(True, act == w)
+        self.assertEquals(False, act == z)
+        self.assertEquals(False, act == x)
+        self.assertEquals(False, act == 4)
+
+    def test_utils_ne(self):
+        act = vector(1, 2)
+        w = vector(1, 2)
+        z = vector(1, 3)
+        x = vector(3, 2)
+        self.assertEquals(False, act != w)
+        self.assertEquals(True, act != z)
+        self.assertEquals(True, act != x)
+        self.assertEquals(True, act != 4)
+
+    def test_utils_iadd(self):
+        exp_x = 4
+        exp_y = 6
+        act = vector(1, 2)
+        w = vector(3, 4)
+        act += w
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        h = hash(act)
+        with self.assertRaises(Exception): act += w
+
+    def test_utils_add(self):
+        exp_x = 4
+        exp_y = 6
+        v = vector(1, 2)
+        w = vector(3, 4)
+        act = v + w
+        self.assertEquals(False, act is v)
+        self.assertEquals(False, act is w)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+
+    def test_utils_isub(self):
+        exp_x = -2
+        exp_y = -2
+        act = vector(1, 2)
+        w = vector(3, 4)
+        act -= w
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        h = hash(act)
+        with self.assertRaises(Exception): act -= w
+
+    def test_utils_sub(self):
+        exp_x = -2
+        exp_y = -2
+        v = vector(1, 2)
+        w = vector(3, 4)
+        act = v - w
+        self.assertEquals(False, act is v)
+        self.assertEquals(False, act is w)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+
+    def test_utils_imul(self):
+        exp_x = 3
+        exp_y = 8
+        act = vector(1, 2)
+        w = vector(3, 4)
+        act *= w
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check multiplying by normal numbers
+        exp_x = 3
+        exp_y = 6
+        act = vector(1, 2)
+        act *= 3
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check hash
+        h = hash(act)
+        with self.assertRaises(Exception): act *= w
+
+    def test_utils_mul(self):
+        exp_x = 3
+        exp_y = 8
+        v = vector(1, 2)
+        w = vector(3, 4)
+        act = v * w
+        self.assertEquals(False, act is v)
+        self.assertEquals(False, act is w)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check multiplying by normal numbers
+        exp_x = 3
+        exp_y = 6
+        v = vector(1, 2)
+        act = v * 3
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check right hand multiplying by normal numbers
+        exp_x = 3
+        exp_y = 6
+        v = vector(1, 2)
+        act = 3 * v
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+
+    def test_utils_scale(self):
+        # Check scaling with vector
+        exp_x = 3
+        exp_y = 8
+        act = vector(1, 2)
+        w = vector(3, 4)
+        act.scale(w)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check scaling with normal numbers
+        exp_x = 3
+        exp_y = 6
+        act = vector(1, 2)
+        act.scale(3)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+
+    def test_utils_itruediv(self):
+        exp_x = 3
+        exp_y = 2
+        act = vector(3, 4)
+        w = vector(1, 2)
+        act /= w
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check dividing by normal numbers
+        exp_x = 1
+        exp_y = 2
+        act = vector(2, 4)
+        act /= 2
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check hash
+        h = hash(act)
+        with self.assertRaises(Exception): act /= w
+
+    def test_utils_truediv(self):
+        exp_x = 3
+        exp_y = 2
+        v = vector(3, 4)
+        w = vector(1, 2)
+        act = v / w
+        self.assertEquals(False, act is v)
+        self.assertEquals(False, act is w)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check dividing by normal numbers
+        exp_x = 1
+        exp_y = 2
+        v = vector(2, 4)
+        act = v /2
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+
+    def test_utils_neg(self):
+        exp_x = -1
+        exp_y = -2
+        v = vector(1, 2)
+        act = -v
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        self.assertEquals(False, v is -v)
+
+    def test_utils_abs(self):
+        exp = 5
+        v = vector(3, 4)
+        act = abs(v)
+        self.assertEquals(exp, act)
+
+    def test_utils_rotate(self):
+        # Check positive rotation
+        exp_x = -2
+        exp_y = 1
+        act = vector(1, 2)
+        act.rotate(90)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+        # Check negative rotation
+        exp_x = 1
+        exp_y = 2
+        act.rotate(-90)
+        self.assertEquals(exp_x, act.x)
+        self.assertEquals(exp_y, act.y)
+
+    def test_utils_repr(self):
+        exp = 'vector(1, 2)'
+        v = vector(1, 2)
+        act = repr(v)
+        self.assertEquals(exp, act)
